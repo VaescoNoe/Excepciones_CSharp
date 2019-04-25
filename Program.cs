59c5e770579a31c4577cc82ec335b72ead7af8d8
Noe <VaescoNoe@gmail.com>
2019-04-25 00:04:08 -0500

    Excepciones

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Procesos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            
+            //Excepciones
+            
+            int valor = 10;
+            Console.WriteLine(valor/0);
+            Excepción no controlada: System.DivideByZeroException: Intento de dividir por cero.
+            
+             */
+
+            // PARSE
+
+            //string valor = "a";
+            //int suma = 4 + int.Parse(valor);
+            //Console.WriteLine(suma);
+
+
+            //string numeroGrande = "99999999999";
+            //Console.WriteLine(int.Parse(numeroGrande));
+
+            int valor = 10;
+            try
+            {
+                Console.WriteLine(valor / 0);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("No se puede dividir un numero sobre 0");
+            }
+
+           
+
+            string valor0 = "a";
+
+
+            try
+            {
+                int suma = 4 + int.Parse(valor0);
+                Console.WriteLine(suma);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("No se puede convertir una letra a numero");
+            }
+            catch (Exception r) {
+                Console.WriteLine("Más de una excepcion");
+            }
+
+
+            try
+            {
+                string numeroGrande = "99999999999";
+                Console.WriteLine(int.Parse(numeroGrande));
+            }
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("El numero no es correcto para el tipo de dato");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
